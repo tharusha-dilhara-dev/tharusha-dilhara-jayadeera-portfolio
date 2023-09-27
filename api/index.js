b27e7b892ce4936fbacdad0244abfee9d71c9f8b
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-09-27 11:09:12 +0530

    add proxy api

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb+srv://tdjayadeera:lXnlQJ1RfyEwwL1X@cluster0.dxd3upl.mo
 
 const allowedOrigins = [
     "http://localhost:5173/",
-    "http://example.com",
+    "https://tdjayadeera.vercel.app/",
 ];
 
 const app=express();
