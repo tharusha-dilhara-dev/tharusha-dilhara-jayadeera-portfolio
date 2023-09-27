404e86950a4b8b015b29e089a664bf0a6931a896
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-09-27 11:55:32 +0530

    add res header

--- api/controllers/getdata.controller.js
+++ api/controllers/getdata.controller.js
@@ -1,5 +1,14 @@
 
 export const getproject = (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
     res.json(
         [
             {
@@ -40,6 +49,15 @@ export const getproject = (req, res) => {
 
 
 export const getcertificate = (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
     res.json(
         [
             {
